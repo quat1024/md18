6642e1f7e26316e8f419239bc86c433960cbaa69
quat1024 <scratch933@gmail.com>
2018-01-22 22:05:34 -0500

    initial

---
+++ src/main/java/quaternary/dazzle/Dazzle.java
@@ -0,0 +1,12 @@
+package quaternary.dazzle;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Dazzle.MODID, name = Dazzle.NAME, version = Dazzle.VERSION)
+public class Dazzle {
+	public static final String MODID = "dazzle";
+	public static final String NAME = "Dazzle";
+	public static final String VERSION = "0.0.0";
+	
+	
+}
