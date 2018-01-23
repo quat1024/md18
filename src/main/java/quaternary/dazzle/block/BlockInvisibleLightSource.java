d37deafa333b00566e0432e2de53e893f15cd4e1
quat1024 <scratch933@gmail.com>
2018-01-22 22:50:45 -0500

    meta light values

--- src/main/java/quaternary/dazzle/block/BlockInvisibleLightSource.java
+++ src/main/java/quaternary/dazzle/block/BlockInvisibleLightSource.java
@@ -2,15 +2,17 @@ package quaternary.dazzle.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.BlockFaceShape;
-import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.*;
 import net.minecraft.util.*;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import quaternary.dazzle.Dazzle;
 
 public class BlockInvisibleLightSource extends Block {
+	
+	public static final PropertyInteger LIGHT_LEVEL = PropertyInteger.create("light_level", 0, 15);
+	
 	public BlockInvisibleLightSource() {
 		//The closest thing to Material.AIR that isn't.
 		super(Material.STRUCTURE_VOID);
@@ -19,6 +21,30 @@ public class BlockInvisibleLightSource extends Block {
 		
 		setRegistryName(new ResourceLocation(Dazzle.MODID, "invisible_light_source"));
 		setUnlocalizedName(Dazzle.MODID + ".invisiblelightsource");
+		
+		setDefaultState(getDefaultState().withProperty(LIGHT_LEVEL, 15));
+	}
+	
+	//Light level based on states
+	@Override
+	public int getLightValue(IBlockState state) {
+		return state.getValue(LIGHT_LEVEL);
+	}
+	
+	//Blockstate boilerplate lol
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return state.getValue(LIGHT_LEVEL);
+	}
+	
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return getDefaultState().withProperty(LIGHT_LEVEL, meta);
+	}
+	
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, LIGHT_LEVEL);
 	}
 	
 	//Make it invisible
@@ -27,7 +53,7 @@ public class BlockInvisibleLightSource extends Block {
 		return EnumBlockRenderType.INVISIBLE;
 	}
 	
-	//make it invisible super for real
+	//Make it invisible super for real
 	@Override
 	public boolean isFullBlock(IBlockState state) {
 		return false;
